fa5ba36083eb6e4bcec44af898f36598501b7a48
gppandey123 <gppandey9919@gmail.com>
2024-06-06 21:12:47 +0530

    section data table implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { SectionData } from './sectionData';
+import { Table } from './table/Table';
 
 function App() {
+  const [section ,setSection] = useState(SectionData?.data?.sections);
+
+  const onChangeInput = (e, itemId, sectionId) => {
+    const { name, value } = e.target
+   
+    setSection(prevSection =>
+      prevSection.map(sections =>
+        sections.section_id === sectionId
+          ? {
+              ...sections,
+              items: sections.items.map(item =>
+                item.item_id === itemId
+                  ? { ...item, [name]: value }
+                  : item
+              )
+            }
+          : sections
+      )
+    );
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {
+        section.map(sectionData => {
+            return (
+              <Table sectionData={sectionData} onChangeInput={onChangeInput}/>
+            )
+        })
+      } 
     </div>
   );
 }
---
+++ src/table/Table.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState }  from 'react';
+import {Icon} from 'react-icons-kit';
+import {eye} from 'react-icons-kit/feather/eye'
+import {check} from 'react-icons-kit/feather/check'
+import './style.css';
+
+export const Table = ({sectionData, onChangeInput}) => {
+
+        const setUpperTotal = sectionData?.items?.reduce((acc ,current)=>  {
+            return acc + current?.quantity*current?.unit_cost;
+        },0);
+       
+    return (
+        <div className="table-container">
+            <div className='section-name'>
+                <p className='name'> - {sectionData?.section_name}</p>
+                <p className='price'> ${setUpperTotal} <Icon class="absolute mr-10" icon={eye} size={18}/></p>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Testype</th>
+                        <th>Item Name</th>
+                        <th>QTY</th>
+                        <th>Unit Cost</th>
+                        <th>Unit</th>
+                        <th>Total</th>
+                        <th>Tax</th>
+                        <th>Cost Code</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        sectionData?.items?.map(item => {
+                            let total = item?.quantity && item?.quantity*item?.unit_cost;
+                            return (
+                                <tr key={item?.item_id}>
+                                <td>{item?.item_type}</td>
+                                <td>{item?.subject}</td>
+                                <td>
+                                     <input
+                                      name="quantity"
+                                      value={item?.quantity}
+                                      type="text"
+                                      onChange={(e) => {onChangeInput(e,item?.item_id, sectionData?.section_id)}}
+                                      placeholder="quantity"
+                                     />
+                                    </td>
+                                <td>
+                                    <input
+                                      name="unit_cost"
+                                      value={item?.unit_cost}
+                                      type="text"
+                                      onChange={(e) => {onChangeInput(e,item?.item_id,sectionData?.section_id)}}
+                                      placeholder="cost"
+                                     />
+                                   
+                                </td>
+                                <td>{item?.unit}</td>
+                                <td>{total || 0}</td>
+                                <td>{item?.tax_rate && <Icon class="absolute mr-10" icon={check} size={18}/>}</td>
+                                <td>{item?.cost_code}</td>
+                                <td><Icon class="absolute mr-10" icon={eye} size={18}/></td>
+                            </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
